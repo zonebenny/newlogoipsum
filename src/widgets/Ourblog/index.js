43641aa690eced25f7aa7e6e87e8aa0e1b7793f8
zone_wac_ui <zone.b@webandcrafts.com>
2024-04-26 09:43:46 +0530

    first updates

--- src/widgets/Ourblog/index.js
+++ src/widgets/Ourblog/index.js
@@ -9,8 +9,8 @@ const Ourblog = () => {
   return (
     <section className='bg-custom-darkgrey-color py-[50px] 2xl:py-[150px]'>
       <div className='container'>
-        <div className='block md:flex justify-between mb-[60px] '>
-          <h4 className='text-3xl 2xl:text-[50px] font-cormorantFont leading-[50px] text-custom-white font-normal mb-[25px] md:mb-0 threeD'>{pagedata?.ourblogsection?.title}</h4>
+        <div className='block md:flex justify-between mb-[60px] OurblogWrap'>
+          <h4 className='text-3xl 2xl:text-[50px] font-cormorantFont leading-[50px] text-custom-white font-normal mb-[25px] md:mb-0 render'>{pagedata?.ourblogsection?.title}</h4>
           <Button href={pagedata?.ourblogsection?.view_all_btn?.url} variant="btnPrimary" outline="outline-primary" name={pagedata?.ourblogsection?.view_all_btn?.text}></Button>
         </div>
           <OurblogCard></OurblogCard>
